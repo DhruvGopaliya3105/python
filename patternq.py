8dfed12b4713056c200bfc5a812372bfe9fd441e
DhruvGopaliya <gopaliyadhruv31@gmail.com>
2026-02-01 23:57:31 +0530

    upload assignment

---
+++ patternq.py
@@ -0,0 +1,34 @@
+# for i in range(1,5):
+#     # print("line",i)
+#     for j in range(1,5):
+#         print("*",end = '')
+#     print( )    
+
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print("*",end = " ")
+#     print(' ')    
+
+# for i in range(1,5):
+#     for j in range(4,i+1,-1):
+#         print("*",end = " ")
+#     print(" ")    
+
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(i,end=" ")
+#     print(" ")     
+
+# for i in range(1,9,2):
+#     for j in range(1,4):
+#         print(i,end=" ")
+#     print(" ")    
+#   for i in range(1,4):
+#     for j in range(5,1,-1):
+#         print(j,end=" ")
+#     print(' ')  
+
+# for i in range(1,5):
+#     for j in range(7,):
+#         print(j,end = " ")
+#     print(" ")    
